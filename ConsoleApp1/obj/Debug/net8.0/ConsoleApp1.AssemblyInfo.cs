8a63d4e5b2e8907cac060d983d2016710e5ccd7d
GoldenBadger159 <mwallentine9291@gmail.com>
2024-04-15 17:51:37 -0600

    repush

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8843abad203d2b7a2dfbd8b81b074c220b4e4998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b93ec3d935604759cdfa70bc55fd224b7893a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
